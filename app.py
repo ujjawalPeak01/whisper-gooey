f24674f76d861e986edd3c98046f459671a04123
Nilesh Agarwal <nick.aggarwal@gmail.com>
2023-05-14 16:02:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class InferlessPythonModel:
         self.generator = pipeline("automatic-speech-recognition", model="vasista22/whisper-hindi-large-v2",device=0)
 
     def infer(self, audio_url, input_language, input_task, input_timestamps):
-        print("Language : " + input_language + " Input task : " + input_task + " Input Timestamp : " + input_timestamps )
+        print("Language : " + input_language + " Input task : " + input_task)
         pipeline_output = self.generator(audio_url)
         generated_txt = pipeline_output["text"]
         return generated_txt
