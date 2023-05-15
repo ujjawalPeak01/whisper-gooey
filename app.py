827cd98c4fabf574eba16ee85c5d7ab0320c6654
Nilesh Agarwal <nick.aggarwal@gmail.com>
2023-05-15 10:01:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,17 @@ import json
 import numpy as np
 import torch
 from transformers import pipeline
+import cv2
 
 
 class InferlessPythonModel:
     def initialize(self):
         self.generator = pipeline("automatic-speech-recognition", model="vasista22/whisper-hindi-large-v2",device=0)
+        url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/overture-creations-5sI6fQgYIuo.png"
+        response = requests.get(url, stream=True).raw
+        arr = np.asarray(bytearray(response.read()), dtype=np.uint8)
+        image = cv2.imdecode(arr, -1)
+        sift = cv2.SIFT_create()
 
     def infer(self, inputs):
         audio_url =  inputs["audio_url"]
